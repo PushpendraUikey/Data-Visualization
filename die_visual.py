1fa7d21cf9e0c9dfd958476fc209c424f0c4b45d
Pushpendra <pushpendrauikey963@gmail.com>
2024-05-19 18:05:39 +0530

    done with simple tasks of visualisation of datas using matplotlib

--- die_visual.py
+++ die_visual.py
@@ -1,6 +1,8 @@
+from matplotlib import axis
 from die import Die
 from plotly.graph_objs import Bar, Layout
 from plotly import offline
+import matplotlib.pyplot as plt
 
 # die = Die()         # six sided die
 # create a D6 and a D10 dice
@@ -9,7 +11,7 @@ die_2 = Die(10)
 # Make some roll no's and store results in a list.
 results = []
 
-for roll_num in range(50000):
+for roll_num in range(5000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -32,3 +34,23 @@ my_layout = Layout(title='Results of rolling a D6 and D10 dice 1000 times',xaxis
 
 offline.plot({'data': data,'layout':my_layout}, filename='D6_D10.html')
 print(frequencies)
+
+"""
+fig, ax = plt.subplots(figsize=(15,9), dpi = 64)
+
+ax.scatter(x_values,frequencies,c='green',s=70)
+plt.show()
+print(frequencies)
+"""
+"""
+fig, ax = plt.subplots()
+ax.plot(x_values,frequencies,linewidth=3)
+
+ax.set_title("Frequency distribution for two roll dice, of D6 and D10",fontsize = 24)
+ax.set_xlabel("Die Value",fontsize=14)
+ax.set_ylabel("Frequency of number", fontsize=14)
+
+ax.tick_params(axis='both',labelsize=14)
+plt.show()
+print(frequencies)
+"""
