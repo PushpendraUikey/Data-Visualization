53bc6200c2c5ab4bc16a6ae27334b99a11be1287
Pushpendra <pushpendrauikey963@gmail.com>
2024-05-19 12:10:06 +0530

    Added simulation for two D6 dices

--- die_visual.py
+++ die_visual.py
@@ -2,29 +2,33 @@ from die import Die
 from plotly.graph_objs import Bar, Layout
 from plotly import offline
 
-die = Die()         # six sided die
-
+# die = Die()         # six sided die
+# create two dice now
+die_1 = Die()
+die_2 = Die()
 # Make some roll no's and store results in a list.
 results = []
 
 for roll_num in range(1001):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # Analyze the results.
 frequencies = []
-for value in range(1,die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2,max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results
-x_values = list(range(1,die.num_sides+1))       #Plotly doesn’t accept the results of the range() function
+x_values = list(range(2,max_result+1))       #Plotly doesn’t accept the results of the range() function
                                                 #directly, so we need to convert the range to a list explicitly using the list()
                                                 #function.
 
 data = [Bar(x=x_values,y=frequencies)]
-x_axis_config = {'title':'Result'}
+x_axis_config = {'title':'Result', 'dtick':1}
 y_axis_config = {'title': 'Frequencies of Result'}
-my_layout = Layout(title='Result of rolling one D6 1000 times',xaxis=x_axis_config, yaxis=y_axis_config)
+my_layout = Layout(title='Results of rolling two D6 1000 times',xaxis=x_axis_config, yaxis=y_axis_config)
 
-offline.plot({'data': data,'layout':my_layout}, filename='D6.html')
+offline.plot({'data': data,'layout':my_layout}, filename='D6_D6.html')
+print(frequencies)
